98476e5cd66cfa470f8ed5ed83fe845f29679fb2
Hirochika Asai <panda@jar.jp>
2015-06-23 01:53:47 +0900

    add fundamental files.

---
+++ tests/basic.c
@@ -0,0 +1,25 @@
+/*_
+ * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Main routine for the basic test
+ */
+int
+main(int argc, const char *const argv[])
+{
+    return 0;
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
