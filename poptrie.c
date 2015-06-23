7fdcb978692f13bfb1c83421d7677a24b3d07648
Hirochika Asai <panda@jar.jp>
2015-06-23 13:17:34 +0900

    implement the initialization function of Poptrie.

--- poptrie.c
+++ poptrie.c
@@ -1,7 +1,164 @@
 /*_
- * Copyright (c) 2015 Hirochika Asai <asai@jar.jp>
+ * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>
  * All rights reserved.
  */
+
+#include "buddy.h"
+#include "poptrie.h"
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Initialize the poptrie data structure
+ */
+struct poptrie *
+poptrie_init(struct poptrie *poptrie, int sz1, int sz0)
+{
+    poptrie_node_t *nodes;
+    poptrie_leaf_t *leaves;
+    int ret;
+
+    if ( NULL == poptrie ) {
+        /* Allocate new one */
+        poptrie = malloc(sizeof(struct poptrie));
+        if ( NULL == poptrie ) {
+            return NULL;
+        }
+        (void)memset(poptrie, 0, sizeof(struct poptrie));
+        /* Set the flag indicating that this data structure needs free() when
+           released. */
+        poptrie->_allocated = 1;
+    } else {
+        /* Write zero's */
+        (void)memset(poptrie, 0, sizeof(struct poptrie));
+    }
+
+    /* Prepare the buddy system for the internal node array */
+    poptrie->cnodes = malloc(sizeof(struct buddy));
+    if ( NULL == poptrie->cnodes ) {
+        poptrie_release(poptrie);
+        return NULL;
+    }
+    ret = buddy_init(poptrie->cnodes, sz1, sz1, sizeof(u32));
+    if ( ret < 0 ) {
+        free(poptrie->cnodes);
+        poptrie->cnodes = NULL;
+        poptrie_release(poptrie);
+        return NULL;
+    }
+
+    /* Prepare the buddy system for the leaf node array */
+    poptrie->cleaves = malloc(sizeof(struct buddy));
+    if ( NULL == poptrie->cleaves ) {
+        poptrie_release(poptrie);
+        return NULL;
+    }
+    ret = buddy_init(poptrie->cleaves, sz0, sz0, sizeof(u32));
+    if ( ret < 0 ) {
+        free(poptrie->cnodes);
+        poptrie->cnodes = NULL;
+        poptrie_release(poptrie);
+        return NULL;
+    }
+
+    /* Prepare the direct pointing array */
+    poptrie->dir = malloc(sizeof(u32) << POPTRIE_S);
+    if ( NULL == poptrie->dir ) {
+        poptrie_release(poptrie);
+        return NULL;
+    }
+
+    /* Prepare the alternative direct pointing array for the update procedure */
+    poptrie->altdir = malloc(sizeof(u32) << POPTRIE_S);
+    if ( NULL == poptrie->altdir ) {
+        poptrie_release(poptrie);
+        return NULL;
+    }
+
+    /* Prepare the FIB mapping table */
+    poptrie->fib.entries = malloc(sizeof(void *) * POPTRIE_INIT_FIB_SIZE);
+    if ( NULL == poptrie->fib.entries ) {
+        poptrie_release(poptrie);
+        return NULL;
+    }
+    poptrie->fib.sz = POPTRIE_INIT_FIB_SIZE;
+    poptrie->fib.n = 0;
+
+    return poptrie;
+}
+
+/*
+ * Release the poptrie data structure
+ */
+void
+poptrie_release(struct poptrie *poptrie)
+{
+    if ( poptrie->cnodes ) {
+        buddy_release(poptrie->cnodes);
+        free(poptrie->cnodes);
+    }
+    if ( poptrie->cleaves ) {
+        buddy_release(poptrie->cleaves);
+        free(poptrie->cleaves);
+    }
+    if ( poptrie->dir ) {
+        free(poptrie->dir);
+    }
+    if ( poptrie->altdir ) {
+        free(poptrie->altdir);
+    }
+    if ( poptrie->fib.entries ) {
+        free(poptrie->fib.entries);
+    }
+    if ( poptrie->_allocated ) {
+        free(poptrie);
+    }
+}
+
+/*
+ * Add a route
+ */
+int
+poptrie_route_add(struct poptrie *poptrie, u32 prefix, int len, u32 nexthop)
+{
+    return -1;
+}
+
+/*
+ * Change a route
+ */
+int
+poptrie_route_change(struct poptrie *poptrie, u32 prefix, int len, u32 nexthop)
+{
+    return -1;
+}
+
+/*
+ * Update a route (add if not exists like BGP update)
+ */
+int
+poptrie_route_update(struct poptrie *poptrie, u32 prefix, int len, u32 nexthop)
+{
+    return -1;
+}
+
+/*
+ * Delete a route
+ */
+int
+poptrie_route_del(struct poptrie *poptrie, u32 prefix, int len)
+{
+    return -1;
+}
+
+/*
+ * Lookup a route by the specified address
+ */
+u32
+poptrie_lookup(struct poptrie *poptrie, u32 addr)
+{
+    return 0;
+}
 
 /*
  * Local variables:
--- poptrie.h
+++ poptrie.h
@@ -1,10 +1,106 @@
 /*_
- * Copyright (c) 2015 Hirochika Asai <asai@jar.jp>
+ * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>
  * All rights reserved.
  */
 
 #ifndef _POPTRIE_H
 #define _POPTRIE_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+
+#define POPTRIE_S               18
+#define POPTRIE_INIT_FIB_SIZE   4096
+
+#define POPCNT(v)               __builtin_popcountll(v)
+#define ZEROCNT(v)              popcnt(~(v))
+#define POPCNT_LS(v, i)         popcnt((v) & (((u64)2 << (i)) - 1))
+#define ZEROCNT_LS(v, i)        popcnt((~(v)) & (((u64)2 << (i)) - 1))
+
+/*
+ * Radix tree node
+ */
+struct radix_node {
+    int valid;
+    struct radix_node *left;
+    struct radix_node *right;
+
+    /* Next hop */
+    u32 prefix;
+    int len;
+
+    /* Propagated route */
+    struct radix_node *exit;
+
+    /* Mark for update */
+    int mark;
+};
+
+/* Internal node */
+typedef struct poptrie_node {
+    u64 leafvec;
+    u64 vector;
+    u32 base0;
+    u32 base1;
+} poptrie_node_t;
+
+/* Leaf node */
+typedef u16 poptrie_leaf_t;
+
+struct poptrie_fib {
+    void **entries;
+    int n;
+    int sz;
+};
+
+/* Poptrie management data structure */
+struct poptrie {
+    /* Root */
+    u32 root;
+
+    /* FIB */
+    struct poptrie_fib fib;
+
+    /* Memory management */
+    void *cnodes;
+    void *cleaves;
+
+    /* Size */
+    int nodesz;
+    int leafsz;
+
+    /* Direct pointing */
+    u32 *dir;
+    u32 *altdir;
+
+    /* RIB */
+    struct radix_node *radix;
+
+    /* Control */
+    int _allocated;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* in poptrie.c */
+    struct poptrie * poptrie_init(struct poptrie *, int, int);
+    void poptrie_release(struct poptrie *);
+    int poptrie_route_add(struct poptrie *, u32, int, u32);
+    int poptrie_route_update(struct poptrie *, u32, int, u32);
+    int poptrie_route_del(struct poptrie *, u32, int);
+    u32 poptrie_lookup(struct poptrie *, u32);
+    u32 poptrie_rt_lookup(struct poptrie *, u32);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _POPTRIE_H */
 
